81820192f7527dd241d2e7f27a9a0a6019195abb
AlperTom <alper.tombulca@web.de>
2025-07-22 23:10:50 +0200

    feat: add production-ready Next.js structure

---
+++ pages/demo.js
@@ -0,0 +1,10 @@
+import ChatBot from '../components/ChatBot'
+
+export default function Demo() {
+  return (
+    <div>
+      <h2>🔧 CarBot-Demo</h2>
+      <ChatBot />
+    </div>
+  );
+}
---
+++ widget/widget.js
@@ -0,0 +1,1 @@
+console.log('CarBot Widget loaded');
