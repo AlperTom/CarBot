85fd2e9fd81e212c3a962923a938bb318bf338a7
AlperTom <alper.tombulca@web.de>
2025-07-23 23:56:04 +0200

    refactor: remove legacy page

--- pages/legal/datenschutz.js
+++
@@ -1,8 +0,0 @@
-export default function Datenschutz() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'Inter, sans-serif' }}>
-      <h1>Datenschutzerklärung</h1>
-      <p>Wir nehmen den Schutz Ihrer Daten ernst...</p>
-    </div>
-  );
-}
