7c88ea9ad3bc02488db96d551852970d5c00ef0f
AlperTom <alper.tombulca@web.de>
2025-07-24 23:39:47 +0200

    fix: dynamic-import client components on home page to avoid prerender error
    Load Hero, FeatureGrid, LeadForm with next/dynamic and ssr:false instead of including them in SSR

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,9 @@
-'use client';
-export const dynamic = 'force-dynamic';
-import Hero from '../components/Hero';
-import FeatureGrid from '../components/FeatureGrid';
-import LeadForm from '../components/LeadForm';
+import dynamic from 'next/dynamic';
+
+// Client-Komponenten dynamisch ohne SSR
+const Hero = dynamic(() => import('../components/Hero'), { ssr: false });
+const FeatureGrid = dynamic(() => import('../components/FeatureGrid'), { ssr: false });
+const LeadForm = dynamic(() => import('../components/LeadForm'), { ssr: false });
 
 export default function Home() {
   return (
