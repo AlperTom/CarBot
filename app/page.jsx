26ab79c9cbc77079956f227236b05e8c112f6bbf
AlperTom <alper.tombulca@web.de>
2025-07-25 00:59:44 +0200

    Update page

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,7 @@
-'use client';
+export const dynamic = 'force-dynamic';
 
-import Hero from '../components/Hero';
-import FeatureGrid from '../components/FeatureGrid';
-import LeadForm from '../components/LeadForm';
+import ClientHome from '../components/ClientHome';
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <FeatureGrid />
-      <LeadForm />
-    </>
-  );
+  return <ClientHome />;
 }
